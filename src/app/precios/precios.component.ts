865f78b835beed6b5be8ba127c35b533cd3e85b7
Sebastian Guarnizo Campos <62730462+sebastian0912@users.noreply.github.com>
2024-04-01 02:56:49 -0500

    Precios -feature- :white_check_mark: pagina precios hecho, quedo responsive

--- src/app/precios/precios.component.ts
+++ src/app/precios/precios.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { NavbarLateralComponent } from '../navbar-lateral/navbar-lateral.component';
+
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-precios',
   standalone: true,
-  imports: [],
+  imports: [NavbarLateralComponent, CommonModule],
   templateUrl: './precios.component.html',
   styleUrl: './precios.component.css'
 })
 export class PreciosComponent {
+
+  pagarMensualidad() {
+
+  }
 
 }
