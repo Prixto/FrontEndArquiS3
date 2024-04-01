8d704e63d11d0de4fee9da05ebbb59d850f6c5a5
Sebastian Guarnizo Campos <62730462+sebastian0912@users.noreply.github.com>
2024-03-31 20:35:44 -0500

    Landing, NavbarLangding, Home - feature - :construction_worker: - Se realizo la creacion de los componentes de navabar superior, landpage, y home, el navar conecta con inicio de sesion, pero en el home aun no hay nada, primero hare la caracteristica del navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,19 @@
 import { Routes } from '@angular/router';
 import path from 'path';
 import { LoginComponent } from './login/login.component';
+import { AppComponent } from './app.component';
+import { LandPageComponent } from './land-page/land-page.component';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
+
     {
-        path: '', component: LoginComponent
+        path: '', component: LandPageComponent
     },
+    {
+        path: 'login', component: LoginComponent
+    },
+    {
+        path: 'home', component: HomeComponent
+    }
 ];
