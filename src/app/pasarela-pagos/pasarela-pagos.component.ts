f9b6e1ee827d45e2c19915b1f81e0362b28d68e4
Sebastian Guarnizo Campos <62730462+sebastian0912@users.noreply.github.com>
2024-04-01 11:23:10 -0500

    Perfil -feature- :construction: Pagina de perfil en proceso

--- src/app/pasarela-pagos/pasarela-pagos.component.ts
+++ src/app/pasarela-pagos/pasarela-pagos.component.ts
@@ -3,6 +3,7 @@ import { NavbarLateralComponent } from '../navbar-lateral/navbar-lateral.compone
 
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-pasarela-pagos',
@@ -12,5 +13,18 @@ import { RouterLink } from '@angular/router';
   styleUrl: './pasarela-pagos.component.css'
 })
 export class PasarelaPagosComponent {
+
+
+  comprar(event: Event) {
+    event.preventDefault(); // Prevenir la acción por defecto del formulario
+    Swal.fire({
+      icon: 'success',
+      title: 'Compra exitosa',
+      text: 'Gracias por su compra',
+      timer: undefined
+    });
+  }
+  
+  
 
 }
--- src/app/perfil/perfil.component.ts
+++ src/app/perfil/perfil.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { NavbarLateralComponent } from '../navbar-lateral/navbar-lateral.component';
+
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-perfil',
   standalone: true,
-  imports: [],
+  imports: [NavbarLateralComponent, CommonModule, RouterLink],
   templateUrl: './perfil.component.html',
   styleUrl: './perfil.component.css'
 })
 export class PerfilComponent {
+    numeroidentificacion = "123456789";
+    nombre = " Luna";
+    apellido = "Mendez";
+    correo = "prueba@gmail.com";
+    tiposervicio = "Gold";
 
 }
